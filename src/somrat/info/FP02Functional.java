1afd8f65a0d32babddc9c2460d1591351551d93c
somratcste <somratcste@gmail.com>
2021-09-06 14:53:29 +0200

    task: use reduce in stream

---
+++ src/somrat/info/FP02Functional.java
@@ -0,0 +1,19 @@
+package somrat.info;
+
+import java.util.List;
+
+public class FP02Functional {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+        int sum = addListFunctional(numbers);
+        System.out.println(sum);
+    }
+
+    private static int sum(int a, int b) {
+        return a + b;
+    }
+    private static int addListFunctional(List<Integer> numbers) {
+        return numbers.stream()
+                .reduce(0, FP02Functional::sum);
+    }
+}
