431e7ccd2546e4c681a9d6a9edb0eac90187af31
somratcste <somratcste@gmail.com>
2021-09-11 01:30:29 +0200

    task: sent function as a parameter

--- src/somrat/info/FP03BehaviorParameterization.java
+++ src/somrat/info/FP03BehaviorParameterization.java
@@ -5,6 +5,7 @@ import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class FP03BehaviorParameterization {
     
@@ -12,6 +13,16 @@ public class FP03BehaviorParameterization {
         List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
         filterAndPrint(numbers, number -> number % 2 == 0);
         filterAndPrint(numbers, number -> number % 2 != 0);
+
+        List<Integer> squaredNumbers = mapAndCreateNewList(numbers, number -> number * number);
+        List<Integer> cubeNumbers = mapAndCreateNewList(numbers, number -> number * number * number);
+    }
+
+    private static List<Integer> mapAndCreateNewList(List<Integer> numbers,
+                                 Function<Integer, Integer> mappingFunction) {
+        return numbers.stream()
+                .map(mappingFunction)
+                .collect(Collectors.toList());
     }
 
     private static void filterAndPrint(List<Integer> numbers, Predicate<Integer> oddPredicate) {
